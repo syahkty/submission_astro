b80f2cf8a4d1af78f2f7e856596ad2b24034c50e
Mujahid <ahmadmujahid343@gmail.com>
2024-04-30 07:47:08 +0800

    wow

---
+++ scripthal2.js
@@ -0,0 +1,24 @@
+const tmblMenuList = document.querySelectorAll('.TombolMenu');
+
+tmblMenuList.forEach(tmblMenu =>{
+    tmblMenu.addEventListener('click', () => {
+        document.querySelector('.TombolTekan')?.classList.remove('TombolTekan');
+        tmblMenu.classList.add('TombolTekan')
+    })
+
+})
+
+const tmblSp = document.getElementById('Sp');
+const tmblMd = document.getElementById('Md');
+const spForm = document.getElementById('Support');
+const mdForm = document.getElementById('mediaShare');
+
+tmblSp.addEventListener('click', function() {
+  spForm.style.display = 'block';
+  mdForm.style.display = 'none';
+});
+
+tmblMd.addEventListener('click', function() {
+  spForm.style.display = 'none';
+  mdForm.style.display = 'block';
+});
